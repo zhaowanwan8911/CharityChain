b5f9bceb52d0e8d8107b70e2e4b2219183929bc3
zww <2578015874@qq.com>
2019-07-19 15:15:58 +0800

    api2

--- app/actions/recipient.js
+++ app/actions/recipient.js
@@ -8,8 +8,8 @@ export const creatRecipient = (params) => {
   return async (dispatch) => {
     try {
       const result = await RestfulAPIUtils.post(API_POST_CREAT_PROJECT, params)
-      if (result.status === 200) {
-        dispatch({ type: types.POST_CREAT_PROJECT, payload: result.data })
+      if (result.status === 201) {
+        dispatch({ type: types.POST_CREAT_PROJECT, payload: { result: "success" } })
       }
     } catch (e) {
       console.error(e)
